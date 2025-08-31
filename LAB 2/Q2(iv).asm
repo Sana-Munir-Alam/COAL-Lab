f46617a3f0d5d0c26bb39152f28be4c493124c2c
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-08-31 19:27:55 +0500

    Update Q2(iv).asm

--- LAB 2/Q2(iv).asm
+++ LAB 2/Q2(iv).asm
@@ -2,7 +2,7 @@ INCLUDE Irvine32.inc
 
 .data
 ; ecx = 101011010110b + 3Ah – 64o + ebx – ecx + 5
-; FOR RIGHT HAND SIDE ecx I am assuming it eds for a moment
+; FOR RIGHT HAND SIDE ecx I am assuming it as EDX for a moment
 counter DD ?
 Var1 DWORD 101011010110b
 Var2 DWORD 3Ah
@@ -17,12 +17,12 @@ main PROC
 mov ebx, Var4	; Storing 27o in ebx
 mov edx, Var5	; Storing 0110b in edx
 
-mov ecx, Var1	; ebx = [101011010110b]
-add ecx, Var2	; ebx = [101011010110b] + 3Ah
-sub ecx, Var3	; ebx = [101011010110b + 3Ah] - 64o
-add ecx, ebx	; ebx = [101011010110b + 3Ah - 64o] + 27o
-sub ecx, edx	; ebx = [101011010110b + 3Ah - 64o + 27o] - 0110b
-add ecx, Var6	; ebx = [101011010110b + 3Ah - 64o + 27o - 0110b] + 5
+mov ecx, Var1	; ecx = [101011010110b]
+add ecx, Var2	; ecx = [101011010110b] + 3Ah
+sub ecx, Var3	; ecx = [101011010110b + 3Ah] - 64o
+add ecx, ebx	; ecx = [101011010110b + 3Ah - 64o] + 27o
+sub ecx, edx	; ecx = [101011010110b + 3Ah - 64o + 27o] - 0110b
+add ecx, Var6	; ecx = [101011010110b + 3Ah - 64o + 27o - 0110b] + 5
 
 ; Storing the calculated value in eax register for it to be printed
 mov eax, ecx
