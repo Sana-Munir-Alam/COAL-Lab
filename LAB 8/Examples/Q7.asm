6592ddf1eb32be443207a82dda6fdedc04c7ff0c
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-10-21 11:54:39 +0500

    Create Q7.asm

---
+++ LAB 8/Examples/Q7.asm
@@ -0,0 +1,25 @@
+INCLUDE Irvine32.inc
+
+.data
+var1 DWORD 5
+var2 DWORD 6
+msg BYTE "The sum of the two numbers is: ", 0
+
+.code
+main PROC
+
+	call AddTwo
+	mov edx, OFFSET msg
+	call WriteString
+	call WriteDec
+	call Crlf
+	exit
+
+main ENDP
+
+AddTwo PROC
+	mov eax, var1
+	add eax, var2
+	ret
+AddTwo ENDP
+END main
