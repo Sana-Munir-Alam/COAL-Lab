69365ea7406b1a316ecb1612b3caf3ac990537c5
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-11-11 11:39:40 +0500

    Update Q1.asm

--- LAB 10/Q1.asm
+++ LAB 10/Q1.asm
@@ -7,21 +7,28 @@ Var3 DWORD 3
 
 .code
 main PROC
-	push Var1
-	push Var2
-	push Var3
-
-	Call ThreeProd
-	exit
+    push Var1
+    push Var2
+    push Var3
+    call ThreeProd
+    add esp, 12          ; clean up (3 parameters × 4 bytes)
+    exit
 main ENDP
 
 ThreeProd PROC
-	mov eax, [esp + 4]    ; Var3 (topmost pushed)
-    mov ebx, [esp + 8]    ; Var2
-    mul ebx               ; EAX = Var3 * Var2
-    mov ecx, [esp + 12]   ; Var1
-    mul ecx               ; EAX = (Var3*Var2)*Var1
+    push ebp
+    mov  ebp, esp
+
+    mov eax, [ebp+8]     ; Var3
+    mov ebx, [ebp+12]    ; Var2
+    mul ebx              ; EAX = Var3 * Var2
+    mov ecx, [ebp+16]    ; Var1
+    mul ecx              ; EAX = (Var3*Var2)*Var1
+
     call WriteDec
+    call Crlf
+
+    pop ebp
     ret
 ThreeProd ENDP
 END main
